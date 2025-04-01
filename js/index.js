ff2251478df0569f7d5ccb05847a2b6da0076328
JuanFerGGz <juanfergg@outlook.com>
2025-04-01 12:47:52 -0500

    variables logged

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 const users = JSON.parse(localStorage.getItem("user")) || [];
-
+const linkCerrar = document.querySelector("#link-cerrar")
+const linkIniciar = document.querySelector("#link-iniciar")
 function verificarUsuario(params) {
     for (let i = 0; i < users.length; i++) {
         if (users[i].logged === true) {
             console.log("puedes cerrar sesion")
+            linkCerrar.style.display = "block"
+            linkIniciar.style.display = "none"
             return
         }
         
     }
     console.log("debes iniciar sesion")
+     linkCerrar.style.display = "none"
+    linkIniciar.style.display = "block"
 }
 verificarUsuario()
