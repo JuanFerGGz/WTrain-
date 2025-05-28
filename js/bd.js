f5fbbe3c364d3ed90c782c6c00226f72f86230b5
JuanFerGGz <juanfergg@outlook.com>
2025-05-28 15:31:00 -0500

    Add files via upload

--- js/bd.js
+++ js/bd.js
@@ -2,56 +2,56 @@ let catalogo=[
     {
         id: 1,
         categoria: "Introducción",
-        descripcion: "Aquí podrás empezar tu curso, se te estará guiando según lo más importante",
+        descripcion: "Aquí podrás empezar tu curso, se te estará guiando según lo más importante.",
         image:"../sources/images/e.png",
         enlace:"../vistas/fitness.html"
     },
     {
         id: 2,
         categoria: "Básicos",
-        descripcion: "Aquí podrás empezar tu curso, se te estará guiando según lo más importante",
+        descripcion: "Verás los ejercicios más básicos para empezar a adaptarte.",
         image:"../sources/images/e.png",
         enlace:"../vistas/basicos.html"
     },
     {
         id: 3,
         categoria: "Salud",
-        descripcion: "Aquí podrás empezar tu curso, se te estará guiando según lo más importante",
+        descripcion: "Conocerás los métodos y las recomendaciones para llevar una vida saludable por medio del fitness.",
         image:"../sources/images/e.png",
         enlace:"../vistas/salud.html"
     },
     {
         id: 4,
         categoria:"Disponibilidad",
-        descripcion: "Aquí podrás empezar tu curso, se te estará guiando según lo más importante",
+        descripcion: "Aprenderás a acoplar los entrenamientos a tu día a día.",
         image:"../sources/images/e.png",
         enlace:"../vistas/disponibilidad.html"
     },
     {
         id: 5,
         categoria: "Técnicas Intermedias",
-        descripcion: "Aquí podrás empezar tu curso, se te estará guiando según lo más importante",
+        descripcion: "Aprenderás variaciones y ejercicios con un mayor grado de dificultad para avanzar.",
         image:"../sources/images/e.png",
         enlace:"../vistas/tecnicas1.html"
     },
     {
         id: 6,
         categoria: "Personalización",
-        descripcion: "Aquí podrás empezar tu curso, se te estará guiando según lo más importante",
+        descripcion: "Aprenderás a armar tu propio plan y rutina de ejercicios modificado segun tu conveniencia.",
         image:"../sources/images/e.png",
         enlace:"../vistas/personalizacion.html"
     },
     {
         id: 7,
         categoria: "Técnicas Avanzadas",
-        descripcion: "Aquí podrás empezar tu curso, se te estará guiando según lo más importante",
+        descripcion: "Aprenderás los ejercicios más avanzados con un alto grado de dificultad.",
         image:"../sources/images/e.png",
         enlace:"../vistas/tecnicas2.html"
     },
     {
         id: 8,
         categoria: "Evaluación",
-        descripcion: "Aquí podrás empezar tu curso, se te estará guiando según lo más importante",
+        descripcion: "Aquí podrás realizar un examen para certificarte en nuestro curso.",
         image:"../sources/images/e.png",
         enlace:"../vistas/evaluacion.html"
     },
